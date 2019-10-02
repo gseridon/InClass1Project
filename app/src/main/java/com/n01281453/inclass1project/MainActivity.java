3fe603867d4124f19261805a1ff925bd04e79955
gseridon <54862042+gseridon@users.noreply.github.com>
2019-10-02 12:55:48 -0400

    - Added a launch screen with a respective theme

--- app/src/main/java/com/n01281453/inclass1project/MainActivity.java
+++ app/src/main/java/com/n01281453/inclass1project/MainActivity.java
@@ -1,18 +1,24 @@
 package com.n01281453.inclass1project;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    // This is a demo
-    // I will add some code here
+    private TextView mTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // The launcher theme is called in the AndroidManifest.xml
+        // Set the default theme before calling super.onCreate
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -25,5 +31,7 @@ public class MainActivity extends AppCompatActivity {
             }
         } );
     }
+
+
 
 }
